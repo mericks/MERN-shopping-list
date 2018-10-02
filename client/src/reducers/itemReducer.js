f168af15889221d255291ed039332af33eb44551
Michelle Erickson <mericks@users.noreply.github.com>
2018-10-02 11:55:13 -0700

    Connect front and back end

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -9,12 +9,14 @@ export default function(state = initialState, action) {
     switch (action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !== action.payload)
+                items: state.items.filter(item => item._id !== action.payload)
             };
         case ADD_ITEM:
             return {
