022cd1c05df9d0e91a464ac37f7d7ec0986d3be5
Michelle Erickson <mericks@users.noreply.github.com>
2018-10-10 15:28:06 -0700

    Create http server & connection file for db events. Move routes to app.js

--- server.js
+++ server.js
@@ -1,38 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-
-const items = require('./lib/routes/api/items');
-
-const app = express();
-
-// No need for BodyParser Middleware when using express.json()
-app.use(express.json());
+const http = require('http');
+const app = require('./lib/app');
+require ('./lib/connection');
 
 
-// DB config
-const dbURI = process.env.DB_URI || 'mongodb://localhost:27017/cheese-force';
-
-//connect to Mongo
-mongoose
-    .connect(dbURI, {useNewUrlParser: true})
-    .then(() => console.log(`MongoDB Connected @${dbURI}...`))
-    .catch(err => console.log(err));
-
-    
-// Use Routes
-app.use('/api/items', items);
-
-// Serve static assets when in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+const server = http.createServer(app);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+server.listen(port, () => {
+    console.log('server running', server.address());
+});
