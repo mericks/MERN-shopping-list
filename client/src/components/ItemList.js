9b92400167f0a8c98e46f6a9348d394d62d5a1ae
Michelle Erickson <mericks@users.noreply.github.com>
2018-10-11 20:23:00 -0700

    Clean up white space on imports

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
-import {
-    Container,
-    ListGroup,
-    ListGroupItem,
-    Button
-} from 'reactstrap';
-import {
-    CSSTransition,
-    TransitionGroup
-} from 'react-transition-group';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
