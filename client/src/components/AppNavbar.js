18aaadeb7669436af34b9f276ab6e8b048606758
Michelle Erickson <mericks@users.noreply.github.com>
2018-10-22 14:26:01 -0700

    Remove AddItemModal from NavBar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container } from 'reactstrap';
-import AddItemModal from './AddItemModal';
+import About from './About';
 
 
 class AppNavbar extends Component {
@@ -23,15 +23,16 @@ class AppNavbar extends Component {
     render () {
         return (
             <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Navbar color="dark" dark expand="sm">
                     <Container>
+                        {/* <img class="navbar-image" src="../assets/cheeseBackground.jpg" alt="Cheese Force" href="/" /> */}
                         <NavbarBrand href="/">Cheese Force</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <AddItemModal />
-                                </NavItem>
+                                {/* <NavItem>
+                                    <About />
+                                </NavItem> */}
                                 <NavItem>
                                     <NavLink href="https://github.com/mericks/cheese-force_MERNFullStack" target="_blank">Source Code</NavLink>
                                 </NavItem>
